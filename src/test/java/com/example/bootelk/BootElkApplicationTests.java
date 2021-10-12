5cace86d59c35079b443abf0a433afdefea7f973
hamzamatloob1997 <=>
2021-10-12 15:06:31 +0500

    initial commit

---
+++ src/test/java/com/example/bootelk/BootElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
